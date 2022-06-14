f29c7aef299c5dd4568156090bd08eda6026cf31
Usuario <Usuario@Usuario-PC>
2022-06-14 17:33:24 -0500

    uploader 1.1

--- CV_EJERCICIO 2/src/cv_ejercicio/pkg2/CV_EJERCICIO2.java
+++ CV_EJERCICIO 2/src/cv_ejercicio/pkg2/CV_EJERCICIO2.java
@@ -17,6 +17,7 @@ public class CV_EJERCICIO2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("version 1");
+        System.out.println("version 1.1");
     }
     
     
